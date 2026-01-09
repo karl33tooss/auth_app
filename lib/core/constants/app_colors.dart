051331a2793b9d7dbf2e8f6b97c1d3f13f8b0992
karl33tooss <karalashik@gmail.com>
2026-01-09 14:42:49 +0200

    Setup folder structure, packages and light theme

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+
+  static const Color scaffoldBackground = Color.fromARGB(255, 246, 246, 249); 
+
+  static const Color primaryBlue = Color.fromARGB(255, 0, 167, 251);
+  static const Color inputFieldBackground = Color.fromARGB(255, 255, 255, 255); 
+  static const Color borderGrey = Color(0xFFE8ECF4);
+  
+  static const Color textMain = Color.fromARGB(255, 36, 36, 36);
+  static const Color textSecondary = Color.fromARGB(255, 115, 115, 115);
+  static const Color textWhite = Color.fromARGB(255, 255, 255, 255);
+  
+  static const LinearGradient primaryGradient = LinearGradient(
+    colors: [
+      Color.fromARGB(255, 35, 105, 246),
+      Color.fromARGB(255, 95, 159, 255),
+    ],
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+  );
+}
